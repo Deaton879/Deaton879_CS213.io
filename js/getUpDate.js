abfa9ccc04ba8ca80fa9a16ed73a8b528c165ef8
Deaton879 <Deaton879@byui.edu>
2020-04-29 17:47:21 -0400

    Overhaul

--- js/getUpDate.js
+++
@@ -1,2 +0,0 @@
-const options ={weekday: 'short', day: 'numeric', month: 'long', year: 'numeric'};
-document.getElementById('todaysDate').textContent = new Date().toLocaleDateString('en-US', options) + " " + "|" +  " " + new Date().toLocaleTimeString(); 
